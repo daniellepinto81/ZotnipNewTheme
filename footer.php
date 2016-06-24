d53259bed35caf0afb927f2e5f5323a39e83dcb7
Mike Pinto <mikepinto81@gmail.com>
2016-06-24 13:31:35 -0400

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content
+ * after. Calls sidebar-footer.php for bottom widgets.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+?>
+	</div><!-- #main -->
+
+
+	<div id="footer" role="contentinfo">
+                
+                
+		
+		<div id="colophon">
+
+<?php
+	/* A sidebar in the footer? Yep. You can can customize
+	 * your footer with four columns of widgets.
+	 */
+	get_sidebar( 'footer' );
+?>
+
+			<div id="site-info">
+			
+				<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<?php bloginfo( 'name' ); ?>
+				</a> 
+				&copy; <?php echo date("Y"); ?> Zotnip LLC. 
+				
+				
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+			
+<br >
+
+                        </div><!-- #site-info -->
+ 
+			<div id="site-generator">
+				
+			</div><!-- #site-generator -->
+
+		</div><!-- #colophon -->
+              <div class="extlinks" style="width: 960px;text-align: center;margin-bottom: 16px;">Looking for other Zotnip web services? <a href="http://zotnipwebservices.com">Click here!</a></div>
+	</div><!-- #footer -->
+
+</div><!-- #wrapper -->
+
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,33 @@
+<?php /** * The Header for our theme. * * Displays all of the <head> section and everything up till <div id="main"> * * @package WordPress * @subpackage Twenty_Ten * @since Twenty Ten 1.0 */ ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>><head>
+        <meta charset="<?php bloginfo('charset'); ?>" />
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+            <?php /* We add some JavaScript to pages with the comment form	 * to support sites with threaded comments (when in use).	 */ if (is_singular() && get_option('thread_comments')) wp_enqueue_script('comment-reply'); /* Always have wp_head() just before the closing </head>	 * tag of your theme, or you will break many plugins, which	 * generally use this hook to add elements to <head> such	 * as styles, scripts, and meta tags.	 */ wp_head(); ?>
+        <script type="text/javascript" src="//use.typekit.net/apa2fui.js"></script>
+            <script type="text/javascript">try {
+        Typekit.load();
+    } catch (e) {
+    }</script> 
+        <link href='http://fonts.googleapis.com/css?family=Press+Start+2P' rel='stylesheet' type='text/css'>
+    </head>
+    <body <?php body_class(); ?>>
+     
+
+        <div id="wrapper" class="hfeed">	
+            <div id="header">
+                <div id="masthead">
+                    <div class="extlinks" style="width: 960px;text-align: center;margin-bottom: 16px;">Looking for other Zotnip web services? <a href="http://zotnipwebservices.com">Click here!</a></div>
+                    <div id="branding" role="banner">
+			<a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><img src="<?php header_image(); ?>" width="<?php echo $header_image_width; ?>" height="<?php echo $header_image_height; ?>" alt="" /></a>	
+                    </div><!-- #branding -->			
+                    <div id="access" role="navigation">
+                         <?php wp_nav_menu(array('container_class' => 'menu-header', 'theme_location' => 'primary')); ?>  
+<!--                       <div class="tagline">
+			</div>-->
+                    </div><!-- #access -->
+                </div><!-- #masthead -->
+            </div><!-- #header -->	
+            <div id="main">
